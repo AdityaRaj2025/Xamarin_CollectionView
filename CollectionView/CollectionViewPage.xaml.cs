b3ed4927eb1572674cb4af18fc0c7ee3ffbe86da
Aditya Raj <adityaraj19997@gmail.com>
2021-11-15 19:53:59 +0530

    first commit

---
+++ CollectionView/CollectionViewPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CollectionViewPage : ContentPage
+    { public IList<Product> Ilist { get; set; }
+        public CollectionViewPage()
+        {
+            InitializeComponent();
+            Ilist=new List<Product>();
+            Ilist.Add(new Product{ ProductName="Laptop",Description="This is sony laptop", ImageURL="Laptop.JPEG"});
+            Ilist.Add(new Product { ProductName = "Led", Description = "This is sony led", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Book", Description = "This is sony book", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Bulb", Description = "This is sony bulb", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Computer", Description = "This is sony computer", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Mouse", Description = "This is sony mouse", ImageURL = "Laptop.JPEG" });
+
+            Ilist.Add(new Product { ProductName = "Modem", Description = "This is sony books", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Sodem", Description = "This is sony bulbs", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Keyboard", Description = "This is sony computers", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Harddisk", Description = "This is sony mouses", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Modem", Description = "This is sony books", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Sodem", Description = "This is sony bulbs", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Keyboard", Description = "This is sony computers", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Product { ProductName = "Harddisk", Description = "This is sony mouses", ImageURL = "Laptop.JPEG" });
+            BindingContext = this;
+        }
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ CollectionView/CollectionViewPage2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CollectionViewPage2 : ContentPage
+    {
+        public IList<Products> Ilist { get; set; }
+        public CollectionViewPage2()
+        {
+            InitializeComponent();
+
+            Ilist = new List<Products>();
+            Ilist.Add(new Products { ProductName = "Laptop", Description = "This is sony laptop", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Led", Description = "This is sony led", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Book", Description = "This is sony book", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Bulb", Description = "This is sony bulb", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Computer", Description = "This is sony computer", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Mouse", Description = "This is sony mouse", ImageURL = "Laptop.JPEG" });
+
+            Ilist.Add(new Products { ProductName = "Modem", Description = "This is sony books", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Sodem", Description = "This is sony bulbs", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Keyboard", Description = "This is sony computers", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Harddisk", Description = "This is sony mouses", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Modem", Description = "This is sony books", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Sodem", Description = "This is sony bulbs", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Keyboard", Description = "This is sony computers", ImageURL = "Laptop.JPEG" });
+            Ilist.Add(new Products { ProductName = "Harddisk", Description = "This is sony mouses", ImageURL = "Laptop.JPEG" });
+            BindingContext = this;
+        }
+    }
+    public class Products
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
